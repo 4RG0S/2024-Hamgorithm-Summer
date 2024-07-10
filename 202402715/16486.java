3757b1462a5c723b3f5ae0096d5566c759280f4f
becky5432 <165632128+becky5432@users.noreply.github.com>
2024-07-11 00:17:05 +0900

    16486번 - 운동장 한 바퀴

---
+++ 202402715/16486.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int d1 = sc.nextInt();
+        int d2 = sc.nextInt();
+        System.out.print(2*d1 + 3.141592*d2*2);
+    }
+}
