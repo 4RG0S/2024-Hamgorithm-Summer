02c7b0ba44f0e7a53c00ab3a28c102f8f6731fe3
alpakaka000808@gmail.com <alpakaka000808@gmail.com>
2024-07-14 00:11:42 +0900

    10610번 - 30

---
+++ 202102655/10610.py
@@ -0,0 +1,19 @@
+# 10610번 - 30
+
+n = input()
+
+if "0" not in n:
+    print(-1)
+
+else:
+    num_sum = 0
+    for i in range(len(n)):
+        num_sum += int(n[i])
+
+    if num_sum % 3 != 0:
+        print(-1)
+    
+    else:
+        sorted_num = sorted(n, reverse=True)
+        answer = "".join(sorted_num)
+        print(answer)
