470ffb7930563ff9df5105242f3a255c99adade5
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2024-07-14 21:10:34 +0900

    5622_이규연

---
+++ 202402712/5622-다이얼.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String S = br.readLine();
+		int count = 0;
+		int k = S.length();
+		
+		for(int i=0;i<k;i++) {
+			
+			switch(S.charAt(i)) {
+			case 'A': case 'B':case'C':
+				count += 3;
+				break;
+			case 'D': case 'E':case'F':
+				count += 4;
+				break;
+			case'G':case'H':case'I':
+				count += 5;
+				break;
+			case'J':case'K':case'L':
+				count += 6;
+				break;
+			case'M':case'N':case'O':
+				count += 7;
+				break;
+			case'P':case'Q':case'R':case'S':
+				count += 8;
+				break;
+			case'T':case'U':case'V':
+				count += 9;
+				break;
+			case'W':case'X':case'Y':case'Z':
+				count += 10;
+				break;
+			}
+			
+		} System.out.println(count);
+		
+	}
+
+}
