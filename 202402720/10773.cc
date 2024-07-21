312c367e90b9b51c91220fe457f3e93b05e635d4
Buntu <github@leebuntu.com>
2024-07-21 22:57:25 +0900

    10773 - 제로

---
+++ 202402720/10773.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int K;
+	cin >> K;
+
+	stack<int> s;
+
+	while (K > 0) {
+		int num;
+		cin >> num;
+
+		if (num == 0) {
+			s.pop();
+		} else {
+			s.push(num);
+		}
+		K--;
+	}
+
+	int result = 0;
+
+	while (!s.empty()) {
+		result += s.top();
+		s.pop();
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
