7e50b4a3c482fa112383651746b899aaef55f4b0
Buntu <github@leebuntu.com>
2024-07-21 22:54:09 +0900

    2720 - 세탁소 사장 동혁

---
+++ 202402720/2720.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	int resultQuarter[T];
+	int resultDime[T];
+	int resultNickel[T];
+	int resultPenny[T];
+
+	for (int i = 0; i < T; i++) {
+		int quarter, dime, nickel, penny;
+
+		int change;
+		cin >> change;
+
+		quarter = change / 25;
+		change = change - quarter * 25;
+		dime = change / 10;
+		change = change - dime * 10;
+		nickel = change / 5;
+		change = change - nickel * 5;
+		penny = change;
+
+		resultQuarter[i] = quarter;
+		resultDime[i] = dime;
+		resultNickel[i] = nickel;
+		resultPenny[i] = penny;
+	}
+
+	for (int i = 0; i < T; i++) {
+		cout << resultQuarter[i] << " " << resultDime[i] << " "
+			 << resultNickel[i] << " " << resultPenny[i] << endl;
+	}
+
+	return 0;
+}
