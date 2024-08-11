39711b7602dae23d35d5885ce7a051f94682be30
alpakaka000808@gmail.com <alpakaka000808@gmail.com>
2024-08-11 21:22:25 +0900

    1911번 - 흙길 보수하기

---
+++ 202102655/1911.py
@@ -0,0 +1,23 @@
+# 1911번 - 흙길보수하기
+
+import sys
+input = sys.stdin.readline
+
+n, l = map(int, input().split())
+pool = [list(map(int, input().split())) for _ in range(n)]
+pool.sort(key=lambda x: x[0])
+plank = pool[0][0]
+total_count = 0
+for start, end in pool:
+    if plank > end:
+        continue
+    elif plank < start:
+        plank = start
+    dist = end - plank
+    remainder = 1
+    if dist % l == 0:
+        remainder = 0
+    count = dist // l + remainder
+    plank += count * l
+    total_count += count
+print(total_count) 
